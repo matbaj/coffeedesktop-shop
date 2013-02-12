b48d62a90c3b4e1187b06ffa667eb0b73233556c
greyfox <greyfox@bshellz.pl>
2013-02-12 07:59:17 +0100

    rails update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CoffeedesktopShop::Application.config.session_store :cookie_store, key: '_coffeedesktop-shop_session'
+CoffeedesktopShop::Application.config.session_store :cookie_store, key: '_coffeedesktop_shop_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
